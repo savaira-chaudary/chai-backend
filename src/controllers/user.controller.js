27baedd76e41a15c33acdf0eb0f0f8018780fbda
Savaira-Chaudary <savairachaudarychaudary@gmail.com>
2025-07-24 19:51:16 +0500

    add getWatchHistory endpoint to fetch user's watch history with video owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,7 +5,7 @@ import { uploadonCloudinary } from '../utils/cloudinary.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import jwt from 'jsonwebtoken'
 import { use } from 'react'
-import { skipMiddlewareFunction } from 'mongoose'
+import mongoose, { skipMiddlewareFunction } from 'mongoose'
 
 const generateAccessAndRefereshTokens = async(userId) =>{
     try {
@@ -402,6 +402,59 @@ return res
 )
 })
 
+const getWatchHistory = asyncHandler(async (req, res)=>{
+   const user = await User.aggregate([
+      {
+         $match:{
+            _id: new mongoose.Types.ObjectId(req.user._id)
+         }
+      },
+      {
+         $lookup:{
+            from: "videos",
+            localField:"watchHistory",
+            foreignField: "_id",
+            as: "watchHistory",
+            pipeline:[
+               {
+                  $lookup:{
+                     from: "users",
+                     localField: "owner",
+                     foreignField:"_id",
+                     as: "owner",
+                     pipeline:[
+                        {
+                           $project:{
+                              fullName: 1,
+                              username: 1,
+                              avatar: 1
+                           }
+                        }
+                     ]
+                  }
+               }
+            ]
+         }
+      },
+      {
+         $addFields:{
+            owner: {
+               $first: "$owner"
+            }
+         }
+      }
+   ])
+   if (!user?.length) {
+      throw new ApiError(404, "user not found")
+   }
+
+   return res
+   .status(200)
+   .json(
+      new ApiResponse(200, user[0].getWatchHistory, "watch history fetched successfully")
+   )
+})
+
 export {
    registerUser,
    loginUser,
@@ -412,5 +465,6 @@ export {
    updateAccountDetails,
    updateUserAvatar,
    updateUserCoverImage,
-   getUserChannelProfile
+   getUserChannelProfile,
+   getWatchHistory
 }
